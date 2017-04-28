7bc7247d96fa0413d0f466c44fc0e0b579d9465b
yxrmine <yxrmine@live.com>
2017-04-28 11:00:58 +0800

    新的first.cpp a 文件夹

---
+++ a/first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello,World! I am " << 18 << " Today!" << endl;
+	int age_number;
+	cin >> age_number;
+	cout << "hello,worlde I am " << age_number << " Today " << endl;
+	return 0;
+}	
+
